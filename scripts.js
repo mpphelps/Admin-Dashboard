00c181a9be03fec43153f9362638283d9328fc82
Michael <mpphelps@gmail.com>
2022-04-29 15:54:59 -0400

    completed project requirements

--- scripts.js
+++ scripts.js
@@ -4,7 +4,24 @@ function setTheme() {
     root.className = newTheme;
     
     document.querySelector('.theme-name').textContent = newTheme;
-  }
+}
   
-  document.querySelector('.theme-toggle-btn').addEventListener('click', setTheme)
-  setTheme();
+document.querySelector('.theme-toggle-btn').addEventListener('click', setTheme)
+setTheme();
+
+
+function linkClicked(e) {
+    if (e.type!=='click') return;
+    this.classList.add('link-clicked');
+    console.log("e");
+}
+
+function removeTransition(e) {
+    if(e.propertyName!=='transform') return; // skip if it's not a transform
+    this.classList.remove('link-clicked');
+}
+
+const dash_title_element = document.querySelector('#dash-title');
+
+dash_title_element.addEventListener("click", linkClicked);
+dash_title_element.addEventListener("transitionend", removeTransition);
